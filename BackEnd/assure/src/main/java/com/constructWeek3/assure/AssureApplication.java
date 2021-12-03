e8ad9c83fa4f97ac2e2e5f077df97102e9c43256
Prottay Ray <prottayray@gmail.com>
2021-12-03 13:15:40 +0530

    testing

--- BackEnd/assure/src/main/java/com/constructWeek3/assure/AssureApplication.java
+++ BackEnd/assure/src/main/java/com/constructWeek3/assure/AssureApplication.java
@@ -8,6 +8,7 @@ public class AssureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssureApplication.class, args);
+		System.out.println("Hello Krishna!");
 	}
 
 }
